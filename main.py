b784c2d43dc7a833aac0dac51cf916931b9d135d
Francis Tseng <f+accounts@frnsys.com>
2019-08-16 12:06:50 -0400

    handling of SKUs

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
+import json
 import stripe
 
 TEST_STRIPE_SECRET_KEY = 'sk_test_...'
 PROD_STRIPE_SECRET_KEY = 'sk_live_...'
+
+def hash_attribs(attribs):
+    return json.dumps(attribs, sort_keys=True)
 
 copy_keys = [
     'active', 'name', 'type',
@@ -13,10 +17,16 @@ copy_keys = [
 stripe.api_key = TEST_STRIPE_SECRET_KEY
 
 products = stripe.Product.list(limit=100, active=True, type='good')['data']
+skus = {}
+for p in products:
+    skus[p.id] = stripe.SKU.list(limit=100, product=p.id)
 products += stripe.Product.list(limit=100, active=True, type='service')['data']
 
 stripe.api_key = PROD_STRIPE_SECRET_KEY
 prod_products = stripe.Product.list(limit=100, active=True, type='good')['data']
+prod_skus = {}
+for p in prod_products:
+    skus[p.id] = {hash_attribs(s['attributes']): s for s in stripe.SKU.list(limit=100, active=True, product=p.id)}
 prod_products += stripe.Product.list(limit=100, active=True, type='service')['data']
 prod_products = {p.name: p for p in prod_products}
 
@@ -24,7 +34,14 @@ for d in products:
     name = d['name']
     kwargs = {k: d[k] for k in copy_keys}
     if name not in prod_products:
-        stripe.Product.create(**kwargs)
+        p = stripe.Product.create(**kwargs)
+        for sku in skus[d.id]:
+            stripe.SKU.create(
+                product=p.id,
+                attributes=sku['attributes'],
+                price=sku['price'],
+                currency=sku['currency'],
+                inventory=sku['inventory'])
     else:
         typ =  kwargs.pop('type')
         if typ != 'good':
@@ -32,3 +49,20 @@ for d in products:
             del kwargs['description']
         p = prod_products[name]
         stripe.Product.modify(p.id, **kwargs)
+        if typ == 'good':
+            for sku in skus[d.id]:
+                key = hash_attribs(sku['attributes'])
+                if key in prod_skus.get(p.id, {}):
+                    stripe.SKU.modify(
+                        prod_skus[p.id][key].id,
+                        attributes=sku['attributes'],
+                        price=sku['price'],
+                        currency=sku['currency'],
+                        inventory=sku['inventory'])
+                else:
+                    stripe.SKU.create(
+                        product=p.id,
+                        attributes=sku['attributes'],
+                        price=sku['price'],
+                        currency=sku['currency'],
+                        inventory=sku['inventory'])
